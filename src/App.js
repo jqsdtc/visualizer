7e6726f2a9c460e8f9def5fe7ad9d2356558b3c4
jqsdtc <jqsdtc@outlook.com>
2020-04-16 13:49:33 -0400

    update router

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Sort from "./pages/sort/Sort";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/pathfinding" component={Path} exact />
